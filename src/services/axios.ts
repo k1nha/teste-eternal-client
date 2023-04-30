7465a993a354044106c5c4267e75c2b1246ccb3d
lucascmpus <lucascmpus@gmail.com>
2023-04-30 14:20:01 -0300

    -

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,7 +4,7 @@ import * as env from 'dotenv';
 env.config();
 
 const instanceAxios: AxiosInstance = axios.create({
-  baseURL: process.env.MY_API || 'http://localhost:3030',
+  baseURL: import.meta.env.MY_API || 'http://localhost:3030',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
