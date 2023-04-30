1351b7dd53cb863f3fe98a1fccb5348bfcd9d026
lucascmpus <lucascmpus@gmail.com>
2023-04-30 14:29:28 -0300

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
       template: {
         transformAssetUrls,
       },
+    }),
+    replace({
+      'process.env': 'import.meta.env',
     }),
 
     quasar({
