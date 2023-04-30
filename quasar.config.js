6e0e58d752a4aed0f6b47e8bdb1197a629746f73
lucascmpus <lucascmpus@gmail.com>
2023-04-30 14:10:43 -0300

    edit quasar

--- quasar.config.js
+++ quasar.config.js
@@ -3,6 +3,6 @@ const { env } = require('process');
 module.exports = function (ctx) {
   css: ['app.scss', 'tailwind.css'];
   env: {
-    API: ctx.dev ? process.env.MY_API : '';
+    API: ctx.dev ? process.env.MY_API : 'http://localhost:3030';
   }
 };
